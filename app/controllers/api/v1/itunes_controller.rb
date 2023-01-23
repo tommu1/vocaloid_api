4006465c0ee7dbb5d29f31f4a7626b1858d2f56f
Tomoya Ishikawa <tomo7623@gmail.com>
2023-01-23 23:56:07 +0900

    件数をクエリで指定

--- app/controllers/api/v1/itunes_controller.rb
+++ app/controllers/api/v1/itunes_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::ItunesController < ApplicationController
   def index
     artist = params[:artist] ? params[:artist] : "DECO27"
+    limit = params[:limit] ? params[:limit] : 20
     musics = ITunesSearchAPI.search(
       term: artist,
       country: 'jp',
       media: 'music',
       lang: 'ja_jp',
-      limit: '20'
+      limit: limit
     )
 
     render json: musics
