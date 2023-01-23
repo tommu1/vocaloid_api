1050bc686658a64f71448d29de9659dcc698ac1b
Tomoya Ishikawa <tomo7623@gmail.com>
2023-01-24 00:00:08 +0900

    コメント追加

--- app/controllers/api/v1/itunes_controller.rb
+++ app/controllers/api/v1/itunes_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ItunesController < ApplicationController
   def index
+
+    #Modelを作成次第、マジックナンバーを定数に置き換える
     artist = params[:artist] ? params[:artist] : "DECO27"
     limit = params[:limit] ? params[:limit] : 20
     musics = ITunesSearchAPI.search(
