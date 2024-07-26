e975cc1b669388b275b1fe8d25f429b8e88f4284
tomoya.ishikawa <tomoya.ishikawa@tomoyaishikawanoMacBook-Pro.local>
2024-07-26 21:42:25 +0900

    albumで検索可能に修正

--- app/controllers/api/v1/itunes_controller.rb
+++ app/controllers/api/v1/itunes_controller.rb
@@ -4,16 +4,14 @@ class Api::V1::ItunesController < ApplicationController
     #Modelを作成次第、マジックナンバーを定数に置き換える
     artist = params[:artist] ? params[:artist] : "DECO27"
     limit = params[:limit] ? params[:limit] : 20
-    # musics = ITunesSearchAPI.search(
-    #   term: artist,
-    #   country: 'jp',
-    #   media: 'music',
-    #   lang: 'ja_jp',
-    #   limit: limit
-    # )
-    musics = ITunesSearchAPI.lookup(
-      id: '353899348',
-      entity: 'album',
+    entity = params[:entity] ? params[:entity] : 'album'
+    musics = ITunesSearchAPI.search(
+      term: artist,
+      country: 'jp',
+      media: 'music',
+      lang: 'ja_jp',
+      limit: limit,
+      entity: entity
     )
     render json: musics
   end
